dace2f53191c2be440f3fef438d91a55ee7f5ddc
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2020-11-13 07:08:00 +0900

    アイコン追加・変更

--- TwitterWhichTvTest/resource.h
+++ TwitterWhichTvTest/resource.h
@@ -2,10 +2,11 @@
 // Microsoft Visual C++ で生成されたインクルード ファイル。
 // Resource.rc で使用
 //
-#define IDB_BITMAP1                     103
 #define IDB_ICON                        103
 #define IDD_TWEET                       105
 #define IDD_SETING                      107
+#define IDB_TWEET_IMG_TEXT              109
+#define IDB_TWEET_IMG_ONLY              110
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON2                     1002
 #define IDC_EDIT1                       1004
@@ -15,7 +16,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
